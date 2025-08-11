742a34a74579b29ac55d5b2d4973d8072e518bce
Zhehuan Chen <50882714+ACMLCZH@users.noreply.github.com>
2025-08-10 20:08:35 -0700

    remove unused bits

--- src/render/vk/memory.cpp
+++ src/render/vk/memory.cpp
@@ -62,12 +62,6 @@ static constexpr VkImageUsageFlags depthAttachmentUsage =
     VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
     VK_IMAGE_USAGE_SAMPLED_BIT;
 
-static constexpr VkImageUsageFlags normalAttachmentUsage =
-    VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
-
-static constexpr VkImageUsageFlags segmentationAttachmentUsage =
-    VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
-
 #if 0
 static constexpr VkImageUsageFlags rtStorageUsage =
     VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
